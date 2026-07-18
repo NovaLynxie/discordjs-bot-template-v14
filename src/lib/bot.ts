85c25af078a6b1a0b2cd5c16dc5db9a7054bb109
Nova P. Lynxie <21307676+NovaLynxie@users.noreply.github.com>
2026-07-18 16:13:45 +0100

    Rewrite events into typescript.

---
+++ src/lib/bot.ts
@@ -0,0 +1,87 @@
+import { customLogger } from './utils/logger';
+import { ActivityType, Client, Collection, GatewayIntentBits } from "discord.js";
+//const config = require("./utils/config").loadConfig(); // call loadConfig() to return config object.
+import { generateCrashReport } from './utils/reports';
+import { readdirSync } from "node:fs";
+import path from "node:path";
+// initialize logger
+const logger = customLogger("main");
+// initialize "client" instance
+export const client = new Client({
+    intents: [
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.Guilds
+        // ADD REQUIRED INTENTS HERE
+    ],
+    presence: {
+        activities: [
+            { name: "custom_status.app_init", state: "Starting up...", type: ActivityType.Custom }
+        ],
+        status: "dnd"
+    }
+});
+// client event handler
+const eventsRootDir = path.join(__dirname, "events");
+for (const file of readdirSync(eventsRootDir).filter((file) => file.endsWith(".js"))) {
+    const eventFilePath = path.resolve(`${eventsRootDir}/${file}`);
+    try {
+        const event = require(eventFilePath);
+        //if (event.name === Events.Debug && config.developer.enableDebug === false) continue;
+        if (event.once) {
+            client.once(event.name, (...args) => event.execute(...args));
+        } else {
+            client.on(event.name, (...args) => event.execute(...args));
+        }
+    } catch(err: any) {
+        logger.error(`EventLoaderError: ${err.message}`);
+        logger.debug(err.stack);
+    }
+}
+// process event handlers
+process.on("SIGINT", () => {
+    logger.warn("Application termination signal received (Ctrl+C)! Stopping processes now.");
+    process.exit(); // gracefully shutdown application here
+});
+process.on("exit", (code) => {
+    logger.info("Stopping bot application...");
+    client.destroy();
+    logger.debug(`Process exited with code "${code}"`);
+    if (code != 0) {
+        logger.warn(`Process exited with non-zero code "${code}"`);
+        logger.warn("Please check previous logs for error details.");
+    } else {
+        logger.info("Application shutdown completed successfully.");
+    }
+});
+process.on("uncaughtException", (err, origin) => {
+    logger.error(`UncaughtExceptionError: ${err.message}`);
+    logger.error(`${err.name}: ${err.message}`);
+    logger.error(`Caused by ${origin}`);
+    logger.debug(err.stack);
+    generateCrashReport(err); // send error to crash report handler
+});
+process.on("unhandledRejection", (err: any, promise) => {
+    logger.error(`UnhandledPromiseRejection: ${err.message}`);
+    logger.error(`${err.name ?? err.type}: ${err.message}`);
+    logger.debug(err.stack);
+    promise.catch((reason) => {
+        logger.debug(reason);
+    });
+});
+client
+    .login(process.env.DISCORD_TOKEN ?? config.application.DISCORD_TOKEN)
+    .then(
+        (token) => {
+            logger.info("Connecting to Discord...");
+            logger.verbose(`DISCORD_TOKEN=${token}`);
+        },
+        (reason) => {
+            logger.error("Failed to connect. Check logs for errors.");
+            if (reason) logger.error(reason); // only log reason if returned
+            process.exit(-1); // bot should stop if login promise rejects
+        }
+    )
+    .catch((err) => {
+        logger.error(err.message);
+        logger.debug(err.stack);
+    });
