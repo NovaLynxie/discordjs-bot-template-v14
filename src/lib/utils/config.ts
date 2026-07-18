5b508e8ad35c9d0eb4d2a7c993dd8c26d7d0f91f
Nova P. Lynxie <21307676+NovaLynxie@users.noreply.github.com>
2026-07-18 16:54:54 +0100

    Rewrite config utils module to typescript.

---
+++ src/lib/utils/config.ts
@@ -0,0 +1,31 @@
+require("dotenv").config({
+    path: (process.env?.NODE_ENV) ? `.env.${process.env?.NODE_ENV}` : '.env',
+    quiet: true
+});
+const logger = require("./logger")("config");
+const { copyFileSync, existsSync, readFileSync } = require("node:fs");
+const toml = require("toml");
+// config module code
+let config: any; // defaults to empty object if no config data returned
+function loadConfig() {
+    try {
+        if (existsSync("./config.toml")) {
+            config = toml.parse(readFileSync("./config.toml", "utf-8"));
+            if (config?.developer?.enableDebug === true) {};
+            return config;
+        } else {
+            logger.warn("Configuration file 'config.toml' was not found in bot directory!");
+            logger.info("Generating default 'config.toml' for first time setup.");
+            copyFileSync("./lib/assets/templates/default_config.toml", "./config.toml");
+            logger.warn("Stopping bot due to missing configuration file. Please configure your bot before restarting it.");
+            process.exit(1);
+        };
+    } catch (err: any) {
+        logger.error("Error occurred while attempting to load './config.toml'!");
+        logger.error(`${err.code}: ${err.message}`);
+        logger.debug(err.stack);
+        process.exit(-1);
+    };
+};
+// export config module namespaces
+module.exports = { ...config, loadConfig };
