ed87728330bd478da56b745e2974a8cf068ee8ea
Nova P. Lynxie <21307676+NovaLynxie@users.noreply.github.com>
2026-09-03 11:18:02 +0100

    Add eslint configuration.

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,31 @@
-// eslint.config.js
+const eslint = require("@eslint/js");
 const { defineConfig } = require("eslint/config");
+const globals = require("globals");
 
 module.exports = defineConfig([
+	eslint.configs.recommended,
 	{
+		files: ["**/*.js"],
+		languageOptions: {
+			ecmaVersion: "latest",
+			sourceType: "commonjs",
+			globals: {
+				...globals.node,
+			},
+		},
 		rules: {
 			semi: "error",
-			"prefer-const": "error",
+			"prefer-const": "warn",
+			"no-console": "warn",
+			"no-constant-condition": ["error", { checkLoops: false }],
+			"no-duplicate-imports": "error",
+			"no-new-wrappers": "error",
+			"no-return-await": "error",
+			"no-unreachable-loop": "error",
+			"no-unused-vars": ["warn", { argsIgnorePattern: "^_", caughtErrors: "none" }],
 		},
+	},
+	{
+		ignores: ["data/**", "logs/**", "node_modules/**"],
 	},
 ]);
